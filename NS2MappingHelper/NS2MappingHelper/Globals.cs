ddf69d68783ca7b2a20a87b1b97fb0ce32649bec
bp2008_cp <bp2008@users.noreply.github.com>
2014-01-30 03:06:02 +0000

    Version 1.0.1

---
+++ NS2MappingHelper/NS2MappingHelper/Globals.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace NS2MappingHelper
+{
+	public static class Globals
+	{
+		private static string executablePath = System.Reflection.Assembly.GetExecutingAssembly().Location.Replace('\\', '/');
+		/// <summary>
+		/// Gets the full path to the current executable file.
+		/// </summary>
+		public static string ExecutablePath
+		{
+			get { return executablePath; }
+		}
+		private static string applicationRoot = new FileInfo(executablePath).Directory.FullName.Replace('\\', '/').TrimEnd('/');
+		/// <summary>
+		/// Gets the full path to the root directory where the current executable is located.  Does not have trailing '/'.
+		/// </summary>
+		public static string ApplicationRoot
+		{
+			get { return applicationRoot; }
+		}
+		private static string applicationDirectoryBase = applicationRoot + "/";
+		/// <summary>
+		/// Gets the full path to the root directory where the current executable is located.  Includes trailing '/'.
+		/// </summary>
+		public static string ApplicationDirectoryBase
+		{
+			get { return applicationDirectoryBase; }
+		}
+		private static string configFilePath = applicationDirectoryBase + "NS2MappingHelper.cfg";
+		/// <summary>
+		/// Gets the full path to the config file.
+		/// </summary>
+		public static string ConfigFilePath
+		{
+			get { return configFilePath; }
+		}
+	}
+}
---
+++ NS2MappingHelper/NS2MappingHelper/NS2MappingHelperConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NS2MappingHelper
+{
+	public class NS2MappingHelperConfiguration : SerializableObjectBase
+	{
+		public string ns2Path = "";
+		public bool useBuilder = true;
+		public string lastOpenMod = "";
+		public List<NS2Mod> mods = new List<NS2Mod>();
+		public NS2MappingHelperConfiguration()
+		{
+		}
+	}
+}
